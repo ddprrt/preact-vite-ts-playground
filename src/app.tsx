3afb21ae2297801a38d981f8802bb6f8c52e519d
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-07-06 13:09:47 +0200

    rough initial

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+import { Fragment, h } from 'preact'
+
+import { Counter } from './Counter'
+import { Logo } from './logo'
+
+export function App() {
+  return (
+    <>
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          With TypeScript!
+        </a>
+      </p>
+      <Counter />
+      <Counter count={1} />
+    </>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+import { h, render } from 'preact'
+
+import { App } from './app'
+
+const el = document.getElementById('app')
+if(el) {
+  render(<App />, el)
+}
+
